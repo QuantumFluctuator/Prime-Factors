e7a0948c9b151b1ac8b33c55f9125878df3eb985
Evan Crabtree <ecrab21@gmail.com>
2019-03-16 23:36:02 +0000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 //  Prime Factorisation
 //
 //  Created by Evan Crabtree on 16/12/2018.
-//  Copyright Â© 2018 QuantumFluctuator. All rights reserved.
+//  Copyright © 2018 QuantumFluctuator. All rights reserved.
 //
 
 #include <iostream>
@@ -11,28 +11,28 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    void primeFactors(int);
-    int n;
-    cout << "Enter number: ";
-    cin >> n;
-    primeFactors(n);
-    cin.get();
-    cin.get();
-    
-    return 0;
+	void primeFactors(long long);
+	long long n;
+	cout << "Enter number: ";
+	cin >> n;
+	primeFactors(n);
+	cin.get();
+	cin.get();
+
+	return 0;
 }
 
-void primeFactors(int number) {
-    while (number % 2 == 0) {
-        cout << 2;
-        printf(" ");
-        number /= 2;
-    }
-    for (int index = 3; index <= number; index += 2) {
-        while (number % index == 0) {
-            cout << index;
-            printf(" ");
-            number /= index;
-        }
-    }
+void primeFactors(long long number) {
+	while (number % 2 == 0) {
+		cout << 2;
+		printf(" ");
+		number /= 2;
+	}
+	for (int index = 3; index <= number; index += 2) {
+		while (number % index == 0) {
+			cout << index;
+			printf(" ");
+			number /= index;
+		}
+	}
 }
